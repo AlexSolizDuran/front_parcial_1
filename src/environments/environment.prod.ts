ae68bced995c80048c8b81e3a35b3040054efba7
AlexSolizDuran <solizelex1999@gmail.com>
2026-04-27 18:20:41 -0400

    vercel2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://auxia-backend.vercel.app',
-  wsUrl: 'wss://auxia-backend.vercel.app/ws',
+  apiUrl: 'https://backend-parcial-1.onrender.com', // Tu URL de Render
+  wsUrl: 'wss://backend-parcial-1.onrender.com'    // Tu URL con wss
 };
