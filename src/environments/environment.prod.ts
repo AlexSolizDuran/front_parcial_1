286fb5461829c8236cc6c633b999c523aeeef53a
AlexSolizDuran <solizelex1999@gmail.com>
2026-04-27 21:57:47 -0400

    vercel3.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://backend-parcial-1.onrender.com', // Tu URL de Render
-  wsUrl: 'wss://backend-parcial-1.onrender.com'    // Tu URL con wss
+  wsUrl: 'wss://backend-parcial-1.onrender.com/ws', // Tu URL con wss
 };
